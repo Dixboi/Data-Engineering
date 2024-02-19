91a59419729bfb69999aa54f50dd4e259d67291b
Dex <59426458+Dixboi@users.noreply.github.com>
2024-02-19 18:36:12 +0800

    Create Variables.cs

---
+++ Bite Projects/C# Learning/Variables.cs
@@ -0,0 +1,19 @@
+
+namespace Variables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			byte number = 2;
+			int count = 10;
+			float totalPrice = 20.95f;
+			char character = 'A';
+			string firstName = "Dex";
+			bool isWorking = true;
+			const float Pi = 3.14f;
+			
+			Console.WriteLine(number);
+		}
+	}
+}
